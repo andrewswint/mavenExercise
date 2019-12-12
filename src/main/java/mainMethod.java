e79989f30179efdc802c008f999dc4d472208388
andrewswint <andrewswint1976@gmail.com>
2019-12-12 15:12:02 -0600

    fixed the output

--- src/main/java/mainMethod.java
+++ src/main/java/mainMethod.java
@@ -9,9 +9,9 @@ public class mainMethod {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter something...");
         String userInput = sc.nextLine();
-        System.out.println(StringUtils.isNumeric(userInput));
-        System.out.println(StringUtils.swapCase(userInput));
-        System.out.println(StringUtils.reverse(userInput));
+        System.out.println("Is a number: " + StringUtils.isNumeric(userInput));
+        System.out.println("Flipped Case: "+ StringUtils.swapCase(userInput));
+        System.out.println("Reversed: " + StringUtils.reverse(userInput));
     }
 
 }
